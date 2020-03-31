d40e628d73529dde0cb2bf486c73baff3902ef4b
Nina Potrebich <potrebich@gmail.com>
2020-03-31 15:35:54 -0700

    Add StyledComponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
+//import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -87,7 +87,7 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
+      //<StyleRoot>
         <div className="App">
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join(' ')}>This is really working!</p>
@@ -96,9 +96,9 @@ class App extends Component {
             onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
         </div>
-      </StyleRoot>
+      //</StyleRoot>
     );
   }
 }
 
-export default Radium(App);
+export default App;
