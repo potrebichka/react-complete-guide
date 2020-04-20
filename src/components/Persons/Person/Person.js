7b6019150ac610ac80f25eb7b58682cb9e4eda2a
Nina Potrebich <potrebich@gmail.com>
2020-04-19 21:09:36 -0700

    Use ref in class component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,13 +6,28 @@ import styles from './Person.module.css';
 import classes from './Person.module.css';
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person] rendering');
         return (
             <React.Fragment>
                 <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} defaultValue={this.props.name}/>
+                <input 
+                    type="text" 
+                    //ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
+                    onChange={this.props.changed} 
+                    defaultValue={this.props.name}
+                />
             </React.Fragment>
         );
     }
