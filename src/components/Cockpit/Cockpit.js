80678cc621209aa11b09d0ab60fa88a6e04ea905
Nina Potrebich <potrebich@gmail.com>
2020-04-19 21:16:22 -0700

    Add refs to functional component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,15 +1,20 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 
 import classes from './Cockpit.module.css';
 
 const Cockpit = (props) => {
+  const toggleButtonRef = useRef(null);
+
+  // after render cycle
+  // [] - first time only
   useEffect(() => {
     console.log('[Cockpit] useEffect');
-    const timer = setTimeout(() => {
-      //alert("Saved data to cloud!");
-    }, 1000);
+    // const timer = setTimeout(() => {
+    //   alert("Saved data to cloud!");
+    // }, 1000);
+    toggleButtonRef.current.click();
     return () => {
-      clearTimeout(timer); 
+      //clearTimeout(timer); 
       console.log('[Cockpit] clean up work')
     }
   }, []);
@@ -41,6 +46,7 @@ const Cockpit = (props) => {
             <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button 
+                ref={toggleButtonRef}
                 className={btnClass}
                 onClick={props.clicked}>Toggle Persons</button>
         </div>
