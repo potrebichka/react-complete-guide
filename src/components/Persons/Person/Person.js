6798ff78c0af71b2d689a9ce6f44cc5828ea9942
Nina Potrebich <potrebich@gmail.com>
2020-04-19 19:38:10 -0700

    Add hoc component withClass

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
+import WithClass from '../../../hoc/WithClass';
 import styles from './Person.module.css';
+import classes from './Person.module.css';
 
 class Person extends Component {
     render() {
         console.log('[Person] rendering');
         return (
-            <React.Fragment>
+            <WithClass classes={classes.Person}>
                 <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} defaultValue={this.props.name}/>
-            </React.Fragment>
+            </WithClass>
         );
     }
 }
