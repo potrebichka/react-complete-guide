07a5ddab05f56f1df3638517da3dbdd7550ec367
Nina Potrebich <potrebich@gmail.com>
2020-04-19 19:25:21 -0700

    Replace hoc with React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,15 @@
 import React, {Component} from 'react';
-import Aux from '../../../hoc/Auxiliary';
 import styles from './Person.module.css';
 
 class Person extends Component {
     render() {
         console.log('[Person] rendering');
         return (
-            <Aux>
+            <React.Fragment>
                 <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} defaultValue={this.props.name}/>
-            </Aux>
+            </React.Fragment>
         );
     }
 }
